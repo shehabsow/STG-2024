73c6d8b4be854c48a0f52cc1f8078722af099e5d
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-07-21 13:43:19 +0300

    Update STG-2024.py

--- STG-2024.py
+++ STG-2024.py
@@ -4,7 +4,7 @@ import pytz
 from datetime import datetime, timedelta
 import json
 import csv
-
+from streamlit_fontawesome import stfa
 st.set_page_config(
     layout="wide",
     page_title='Earthquake analysis',
