51dfb36078370822eb0d1309727708edd28c050a
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-07-23 11:35:21 +0300

    Update STG-2024.py

--- STG-2024.py
+++ STG-2024.py
@@ -180,7 +180,7 @@ else:
         page = st.sidebar.radio('Select page', ['area', 'View Logs'])
         
         if page == 'area':
-            col1, col2= st.columns([1,100000])
+            col2= st.columns([100000])
             with col2:
                 def main():
                     st.markdown("""
