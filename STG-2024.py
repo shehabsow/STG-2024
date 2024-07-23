aa79e6a2fa147b28c97409ed2d18c4d70764eccd
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-07-23 11:30:57 +0300

    Update STG-2024.py

--- STG-2024.py
+++ STG-2024.py
@@ -169,97 +169,97 @@ else:
         
         # قراءة البيانات
    
-    if 'df' not in st.session_state:
-        st.session_state.df = pd.read_csv('matril.csv')
-    try:
-        logs_df = pd.read_csv('logs.csv')
-        st.session_state.logs = logs_df.to_dict('records')
-    except FileNotFoundError:
-        st.session_state.logs = []
+if 'df' not in st.session_state:
+    st.session_state.df = pd.read_csv('matril.csv')
+try:
+    logs_df = pd.read_csv('logs.csv')
+    st.session_state.logs = logs_df.to_dict('records')
+except FileNotFoundError:
+    st.session_state.logs = []
 
-    page = st.sidebar.radio('Select page', ['area', 'View Logs'])
+page = st.sidebar.radio('Select page', ['area', 'View Logs'])
 
-    if page == 'area':
-        col1, col2, col3 = st.columns([1,100000,1])
-        with col2:
-            def main():
+if page == 'area':
+    col1, col2, col3 = st.columns([1,100000,1])
+    with col2:
+        def main():
+            st.markdown("""
+            <style>
+                .stProgress > div > div > div {
+                    background-color: #FFD700;
+                    border-radius: 50%;
+                }
+            </style>
+            """, unsafe_allow_html=True)
+            
+            with st.spinner("Data loaded successfully!"):
+                import time
+                time.sleep(1)
+            
+            col1, col2 = st.columns([2, 0.75])
+            with col1:
                 st.markdown("""
-                <style>
-                    .stProgress > div > div > div {
-                        background-color: #FFD700;
-                        border-radius: 50%;
-                    }
-                </style>
+                    <h2 style='text-align: center; font-size: 40px; color: red;'>
+                        Find your Mechanical parts
+                    </h2>
                 """, unsafe_allow_html=True)
-                
-                with st.spinner("Data loaded successfully!"):
-                    import time
-                    time.sleep(1)
-                
-                col1, col2 = st.columns([2, 0.75])
-                with col1:
-                    st.markdown("""
-                        <h2 style='text-align: center; font-size: 40px; color: red;'>
-                            Find your Mechanical parts
-                        </h2>
-                    """, unsafe_allow_html=True)
-                
-                with col2:
-                    search_keyword = st.session_state.get('search_keyword', '')
-                    search_keyword = st.text_input("Enter keyword to search:", search_keyword)
-                    search_button = st.button("Search")
-                    search_option = 'All Columns'
-                
-                def search_in_dataframe(df_f, keyword, option):
-                    if option == 'All Columns':
-                        result = df_f[df_f.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
-                    else:
-                        result = df_f[df_f[option].astype(str).str.contains(keyword, case=False)]
-                    return result
-                
-                if st.session_state.get('refreshed', False):
-                    st.session_state.search_keyword = ''
-                    st.session_state.refreshed = False
-                
-                if search_button and search_keyword:
-                    st.session_state.search_keyword = search_keyword
-                    search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
-                    st.write(f"Search results for '{search_keyword}' in {search_option}:")
-                    st.dataframe(search_results, width=1000, height=200)
-                st.session_state.refreshed = True 
-                
-                tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
-                    'Reel for Item Label (Small)', 'Reel for Item Label (Large)',
-                    'Ink Reels for Item Label', 'Red Tape', 'Adhesive Tape', 'Cartridges', 'MultiPharma Cartridge'
-                ])
-                
-                with tab1:
-                    peraing = df_f[df_f['Item Name'] == 'Reel for Item Label (Small)'].sort_values(by='Item Name')
-                    st.dataframe(peraing,width=2000)
-                    display_tab('Reel for Item Label (Small)', 100)
-                   
-                with tab2:
-                    display_tab('Reel for Item Label (Large)', 200)
-                with tab3:
-                    display_tab('Ink Reels for Item Label', 150)
-                with tab4:
-                    display_tab('Red Tape', 50)
-                with tab5:
-                    display_tab('Adhesive Tape', 75)
-                with tab6:
-                    display_tab('Cartridges', 80)
-                with tab7:
-                    display_tab('MultiPharma Cartridge', 120)
+            
+            with col2:
+                search_keyword = st.session_state.get('search_keyword', '')
+                search_keyword = st.text_input("Enter keyword to search:", search_keyword)
+                search_button = st.button("Search")
+                search_option = 'All Columns'
+            
+            def search_in_dataframe(df_f, keyword, option):
+                if option == 'All Columns':
+                    result = df_f[df_f.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
+                else:
+                    result = df_f[df_f[option].astype(str).str.contains(keyword, case=False)]
+                return result
+            
+            if st.session_state.get('refreshed', False):
+                st.session_state.search_keyword = ''
+                st.session_state.refreshed = False
+            
+            if search_button and search_keyword:
+                st.session_state.search_keyword = search_keyword
+                search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
+                st.write(f"Search results for '{search_keyword}' in {search_option}:")
+                st.dataframe(search_results, width=1000, height=200)
+            st.session_state.refreshed = True 
+            
+            tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
+                'Reel for Item Label (Small)', 'Reel for Item Label (Large)',
+                'Ink Reels for Item Label', 'Red Tape', 'Adhesive Tape', 'Cartridges', 'MultiPharma Cartridge'
+            ])
+            
+            with tab1:
+                peraing = df_f[df_f['Item Name'] == 'Reel for Item Label (Small)'].sort_values(by='Item Name')
+                st.dataframe(peraing,width=2000)
+                display_tab('Reel for Item Label (Small)', 100)
+               
+            with tab2:
+                display_tab('Reel for Item Label (Large)', 200)
+            with tab3:
+                display_tab('Ink Reels for Item Label', 150)
+            with tab4:
+                display_tab('Red Tape', 50)
+            with tab5:
+                display_tab('Adhesive Tape', 75)
+            with tab6:
+                display_tab('Cartridges', 80)
+            with tab7:
+                display_tab('MultiPharma Cartridge', 120)
 
-                if st.session_state.alerts:
-                    st.error(f"Low stock for items: {', '.join(st.session_state.alerts)}")
+            if st.session_state.alerts:
+                st.error(f"Low stock for items: {', '.join(st.session_state.alerts)}")
 
-            main()
+        main()
 
-    elif page == 'View Logs':
-        st.header('Logs')
-        if st.session_state.logs:
-            logs_df = pd.DataFrame(st.session_state.logs)
-            st.dataframe(logs_df, width=1000, height=400)
-        else:
-            st.write("No logs available.")
+elif page == 'View Logs':
+    st.header('Logs')
+    if st.session_state.logs:
+        logs_df = pd.DataFrame(st.session_state.logs)
+        st.dataframe(logs_df, width=1000, height=400)
+    else:
+        st.write("No logs available.")
