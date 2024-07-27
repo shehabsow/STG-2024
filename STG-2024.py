eb17bf7d7ed30c7853a6d7a55ea90a7140c97ffc
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-07-27 10:05:54 +0300

    Update STG-2024.py

--- STG-2024.py
+++ STG-2024.py
@@ -190,122 +190,122 @@ else:
         
         # قراءة البيانات
            
-    if 'df' not in st.session_state:
-        st.session_state.df = pd.read_csv('matril.csv')
-    try:
-        logs_df = pd.read_csv('logs.csv')
-        st.session_state.logs = logs_df.to_dict('records')
-    except FileNotFoundError:
-        st.session_state.logs = []
-    st.markdown("""
-            <style>
-                .main {
-                    padding: 0rem 1rem;
-                    width: 200%;
-                }
-                .stApp {
-                    overflow: hidden;
-                }
-            </style>
-            """, unsafe_allow_html=True)
-    
-             
-    page = st. sidebar.radio('Select page', ['STG-2024', 'View Logs'])
-    
-    if page == 'STG-2024':
-        def main():
-            st.markdown("""
-            <style>
-                .stProgress > div > div > div {
-                    background-color: #FFD700;
-                    border-radius: 50%;
-                }
-            </style>
-            """, unsafe_allow_html=True)
-            
-            with st.spinner("Data loaded successfully!"):
-                import time
-                time.sleep(1)
-            
-            col1, col2 = st.columns([2, 0.75])
-            with col1:
-                st.markdown("""
-                    <h2 style='text-align: center; font-size: 40px; color: red;'>
-                        Find your parts
-                    </h2>
+        if 'df' not in st.session_state:
+            st.session_state.df = pd.read_csv('matril.csv')
+        try:
+            logs_df = pd.read_csv('logs.csv')
+            st.session_state.logs = logs_df.to_dict('records')
+        except FileNotFoundError:
+            st.session_state.logs = []
+        st.markdown("""
+                <style>
+                    .main {
+                        padding: 0rem 1rem;
+                        width: 200%;
+                    }
+                    .stApp {
+                        overflow: hidden;
+                    }
+                </style>
                 """, unsafe_allow_html=True)
-            
-            with col2:
-                search_keyword = st.session_state.get('search_keyword', '')
-                search_keyword = st.text_input("Enter keyword to search:", search_keyword)
-                search_button = st.button("Search")
-                search_option = 'All Columns'
-            
-            def search_in_dataframe(df_f, keyword, option):
-                if option == 'All Columns':
-                    result = df_f[df_f.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
-                else:
-                    result = df_f[df_f[option].astype(str).str.contains(keyword, case=False)]
-                return result
-            
-            if st.session_state.get('refreshed', False):
-                st.session_state.search_keyword = ''
-                st.session_state.refreshed = False
-            
-            if search_button and search_keyword:
-                st.session_state.search_keyword = search_keyword
-                search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
-                st.write(f"Search results for '{search_keyword}' in {search_option}:")
-                st.dataframe(search_results, width=1000, height=200)
-            st.session_state.refreshed = True 
-            
-            tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
-                'Reel Label (Small)', 'Reel Label (Large)',
-                'Ink Reels for Label', 'Red Tape', 'Adhasive Tape', 'Cartridges', 'MultiPharma Cartridge'
-            ])
-            
-            with tab1:
-                peraing = df_Material[df_Material['Item Name'] == 'Reel Label (Small)'].sort_values(by='Item Name')
-                st.dataframe(peraing,width=2000)
-                display_tab('Reel Label (Small)', 100)
-               
-            with tab2:
-                peraing = df_Material[df_Material['Item Name'] == 'Reel Label (Large)'].sort_values(by='Item Name')
-                st.dataframe(peraing,width=2000)
-                display_tab('Reel Label (Large)', 200)
-            with tab3:
-                peraing = df_Material[df_Material['Item Name'] == 'Ink Reels for Label'].sort_values(by='Item Name')
-                st.dataframe(peraing,width=2000)
-                display_tab('Ink Reels for Label', 150)
-            with tab4:
-                peraing = df_Material[df_Material['Item Name'] == 'Red Tape'].sort_values(by='Item Name')
-                st.dataframe(peraing,width=2000)
-                display_tab('Red Tape', 50)
-            with tab5:
-                peraing = df_Material[df_Material['Item Name'] == 'Adhasive Tape'].sort_values(by='Item Name')
-                st.dataframe(peraing,width=2000)
-                display_tab('Adhasive Tape', 75)
-            with tab6:
-                peraing = df_Material[df_Material['Item Name'] == 'Cartridges'].sort_values(by='Item Name')
-                st.dataframe(peraing,width=2000)
-                display_tab('Cartridges', 80)
-            with tab7:
-                peraing = df_Material[df_Material['Item Name'] == 'MultiPharma Cartridge'].sort_values(by='Item Name')
-                st.dataframe(peraing,width=2000)
-                display_tab('MultiPharma Cartridge', 120)
-    
-            
-    
-        if __name__ == '__main__':
-            main()
-    
-    elif page == 'View Logs':
-        st.header('User Activity Logs')
-        if st.session_state.logs:
-            logs_df = pd.DataFrame(st.session_state.logs)
-            st.dataframe(logs_df, width=1000, height=400)
-            csv = logs_df.to_csv(index=False)
-            st.download_button(label="Download Logs as sheet", data=csv, file_name='user_logs.csv', mime='text/csv')
-        else:
-            st.write("No logs available.")
-  
+        
+                 
+        page = st. sidebar.radio('Select page', ['STG-2024', 'View Logs'])
+        
+        if page == 'STG-2024':
+            def main():
+                st.markdown("""
+                <style>
+                    .stProgress > div > div > div {
+                        background-color: #FFD700;
+                        border-radius: 50%;
+                    }
+                </style>
+                """, unsafe_allow_html=True)
+                
+                with st.spinner("Data loaded successfully!"):
+                    import time
+                    time.sleep(1)
+                
+                col1, col2 = st.columns([2, 0.75])
+                with col1:
+                    st.markdown("""
+                        <h2 style='text-align: center; font-size: 40px; color: red;'>
+                            Find your parts
+                        </h2>
+                    """, unsafe_allow_html=True)
+                
+                with col2:
+                    search_keyword = st.session_state.get('search_keyword', '')
+                    search_keyword = st.text_input("Enter keyword to search:", search_keyword)
+                    search_button = st.button("Search")
+                    search_option = 'All Columns'
+                
+                def search_in_dataframe(df_f, keyword, option):
+                    if option == 'All Columns':
+                        result = df_f[df_f.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
+                    else:
+                        result = df_f[df_f[option].astype(str).str.contains(keyword, case=False)]
+                    return result
+                
+                if st.session_state.get('refreshed', False):
+                    st.session_state.search_keyword = ''
+                    st.session_state.refreshed = False
+                
+                if search_button and search_keyword:
+                    st.session_state.search_keyword = search_keyword
+                    search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
+                    st.write(f"Search results for '{search_keyword}' in {search_option}:")
+                    st.dataframe(search_results, width=1000, height=200)
+                st.session_state.refreshed = True 
+                
+                tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
+                    'Reel Label (Small)', 'Reel Label (Large)',
+                    'Ink Reels for Label', 'Red Tape', 'Adhasive Tape', 'Cartridges', 'MultiPharma Cartridge'
+                ])
+                
+                with tab1:
+                    peraing = df_Material[df_Material['Item Name'] == 'Reel Label (Small)'].sort_values(by='Item Name')
+                    st.dataframe(peraing,width=2000)
+                    display_tab('Reel Label (Small)', 100)
+                   
+                with tab2:
+                    peraing = df_Material[df_Material['Item Name'] == 'Reel Label (Large)'].sort_values(by='Item Name')
+                    st.dataframe(peraing,width=2000)
+                    display_tab('Reel Label (Large)', 200)
+                with tab3:
+                    peraing = df_Material[df_Material['Item Name'] == 'Ink Reels for Label'].sort_values(by='Item Name')
+                    st.dataframe(peraing,width=2000)
+                    display_tab('Ink Reels for Label', 150)
+                with tab4:
+                    peraing = df_Material[df_Material['Item Name'] == 'Red Tape'].sort_values(by='Item Name')
+                    st.dataframe(peraing,width=2000)
+                    display_tab('Red Tape', 50)
+                with tab5:
+                    peraing = df_Material[df_Material['Item Name'] == 'Adhasive Tape'].sort_values(by='Item Name')
+                    st.dataframe(peraing,width=2000)
+                    display_tab('Adhasive Tape', 75)
+                with tab6:
+                    peraing = df_Material[df_Material['Item Name'] == 'Cartridges'].sort_values(by='Item Name')
+                    st.dataframe(peraing,width=2000)
+                    display_tab('Cartridges', 80)
+                with tab7:
+                    peraing = df_Material[df_Material['Item Name'] == 'MultiPharma Cartridge'].sort_values(by='Item Name')
+                    st.dataframe(peraing,width=2000)
+                    display_tab('MultiPharma Cartridge', 120)
+        
+                
+        
+            if __name__ == '__main__':
+                main()
+        
+        elif page == 'View Logs':
+            st.header('User Activity Logs')
+            if st.session_state.logs:
+                logs_df = pd.DataFrame(st.session_state.logs)
+                st.dataframe(logs_df, width=1000, height=400)
+                csv = logs_df.to_csv(index=False)
+                st.download_button(label="Download Logs as sheet", data=csv, file_name='user_logs.csv', mime='text/csv')
+            else:
+                st.write("No logs available.")
+      
