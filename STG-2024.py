fc89e93ad9f0b1be9755b2bfbbb37c2c65fad6d8
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-08-06 16:29:08 +0300

    Update STG-2024.py

--- STG-2024.py
+++ STG-2024.py
@@ -8,6 +8,8 @@ import os
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import requests
+import msal
 st.set_page_config(
     layout="wide",
     page_title='STG-2024',
