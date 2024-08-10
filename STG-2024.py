e04742cd65adf632c93fb1c93912df6cb26ecc11
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-08-10 18:07:29 +0300

    Update STG-2024.py

--- STG-2024.py
+++ STG-2024.py
@@ -196,8 +196,8 @@ if not st.session_state.logged_in:
     with col2:
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
-        st.button("Login")
-        login(username, password)
+        if st.button("Login"):
+            login(username, password)
 else:
     if st.session_state.first_login:
         col1, col2, col3 = st.columns([1, 1, 1])
