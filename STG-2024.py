fc25b30b4cd931ed7d04d17d88cfa9ad14d77331
shehabsow <140345568+shehabsow@users.noreply.github.com>
2024-08-29 11:58:18 +0300

    Update STG-2024.py

--- STG-2024.py
+++ STG-2024.py
@@ -3,11 +3,7 @@ import pandas as pd
 import pytz
 from datetime import datetime, timedelta
 import json
-import csv
 import os
-import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
 st.set_page_config(
     layout="wide",
@@ -16,8 +12,6 @@ st.set_page_config(
 
 egypt_tz = pytz.timezone('Africa/Cairo')
 df_Material = pd.read_csv('matril.csv')
-#df_BIN = pd.read_csv('LOCATION.csv')
-#df_Receving = pd.read_csv('Receving.csv')
 
 # Load users data
 def load_users():
@@ -27,9 +21,9 @@ def load_users():
     except (FileNotFoundError, json.JSONDecodeError):
         return {
             "knhp322": {"password": "knhp322", "first_login": True, "name": "Shehab Ayman", "last_password_update": str(datetime.now(egypt_tz))},
-            "KFXW551": {"password": "KFXW551", "first_login": True, "name": " Hossameldin Mostafa", "last_password_update": str(datetime.now(egypt_tz))},
-            "knvp968": {"password": "knvp968", "first_login": True, "name": "  Mohamed Nader", "last_password_update": str(datetime.now(egypt_tz))},
-            "kcqw615": {"password": "kcqw615", "first_login": True, "name": "Tareek Mahmoud ", "last_password_update": str(datetime.now(egypt_tz))}}
+            "KFXW551": {"password": "KFXW551", "first_login": True, "name": "Hossameldin Mostafa", "last_password_update": str(datetime.now(egypt_tz))},
+            "knvp968": {"password": "knvp968", "first_login": True, "name": "Mohamed Nader", "last_password_update": str(datetime.now(egypt_tz))},
+            "kcqw615": {"password": "kcqw615", "first_login": True, "name": "Tareek Mahmoud", "last_password_update": str(datetime.now(egypt_tz))}}
 
 # Save users data to JSON file
 def save_users(users):
@@ -58,22 +52,7 @@ def login(username, password):
     else:
         st.error("Incorrect username or password")
 
-# Update password function
-def update_password(username, new_password, confirm_new_password):
-    if new_password == confirm_new_password:
-        users[username]["password"] = new_password
-        users[username]["first_login"] = False
-        users[username]["last_password_update"] = str(datetime.now(egypt_tz))
-        save_users(users)
-        st.session_state.first_login = False
-        st.session_state.password_expired = False
-        st.success("Password updated successfully!")
-    else:
-        st.error("Passwords do not match!")
-
-
-        
-# دالة لتحديث الكمية
+# Update quantity function
 def update_quantity(row_index, quantity, operation, username):
     last_month = st.session_state.df.loc[row_index, 'Actual Quantity']
     if operation == 'add':
@@ -93,33 +72,30 @@ def update_quantity(row_index, quantity, operation, username):
     }
     st.session_state.logs.append(log_entry)
     
-    # حفظ السجلات إلى ملف CSV
+    # Save logs to CSV
     logs_df = pd.DataFrame(st.session_state.logs)
     logs_df.to_csv('logs.csv', index=False)
     
-    # تحقق من الكميات وتحديث التنبيهات
+    # Check quantities and update alerts
     check_quantities()
-
 
 def check_quantities():
     new_alerts = []
     for index, row in st.session_state.df.iterrows():
-        if row['Actual Quantity'] < 100:  # تغيير القيمة حسب الحاجة
+        if row['Actual Quantity'] < 100:  # Change the threshold as needed
             new_alerts.append(row['Item Name'])
             
     st.session_state.alerts = new_alerts
     save_alerts(st.session_state.alerts)
 
-# دالة للتحقق من الكميات لكل تبويب وعرض التنبيهات
+# Function to check quantities for each tab and display alerts
 def check_tab_quantities(tab_name, min_quantity):
-    
     df_tab = st.session_state.df[st.session_state.df['Item Name'] == tab_name]
     tab_alerts = df_tab[df_tab['Actual Quantity'] < min_quantity]['Item Name'].tolist()
    
-    
     return tab_alerts, df_tab
 
-# عرض التبويبات
+# Function to display each tab
 def display_tab(tab_name, min_quantity):
     st.header(f'{tab_name}')
     row_number = st.number_input(f'Select row number for {tab_name}:', min_value=0, max_value=len(st.session_state.df)-1, step=1, key=f'{tab_name}_row_number')
@@ -140,7 +116,6 @@ def display_tab(tab_name, min_quantity):
         st.error(f"Low stock for items in {tab_name}:")
         st.dataframe(df_tab.style.applymap(lambda x: 'background-color: red' if x < min_quantity else '', subset=['Actual Quantity']))
 
-
 def clear_logs():
     st.session_state.logs = []
     logs_df = pd.DataFrame(columns=['user', 'time', 'item', 'old_quantity', 'new_quantity', 'operation'])
@@ -149,161 +124,146 @@ def clear_logs():
     
 users = load_users()
 
-# واجهة تسجيل الدخول
+# Login Interface
 if 'logged_in' not in st.session_state:
     st.session_state.logged_in = False
     st.session_state.logs = []
 
 if 'logs' not in st.session_state:
-    st.session_state.logs = load_logs()[0]
-    
+    st.session_state.logs = []
+
 if not st.session_state.logged_in:
     col1, col2, col3 = st.columns([1, 1, 1])
     with col2:
         username = st.text_input("Username")
         password = st.text_input("Password", type="password")
-        st.button("Login")
-        login(username, password)
+        if st.button("Login"):
+            login(username, password)
 else:
-    if st.session_state.first_login:
-        col1, col2, col3 = st.columns([1, 1, 1])
-        with col2:
-            st.subheader("Change Password")
-            new_password = st.text_input("New Password", type="password")
-            confirm_new_password = st.text_input("Confirm Password", type="password")
-            if st.button("Change Password"):
-                if not new_password or not confirm_new_password:
-                    st.error("Please fill in all the fields.")
-                else:
-                    update_password(st.session_state.username, new_password, confirm_new_password)
-    else:
-        st.markdown(f"<div style='text-align: right; font-size: 20px; color: green;'>Logged in by: {users[st.session_state.username]['name']}</div>", unsafe_allow_html=True)
-        
-        # قراءة البيانات
-           
-        if 'df' not in st.session_state:
-            st.session_state.df = pd.read_csv('matril.csv')
-        try:
-            logs_df = pd.read_csv('logs.csv')
-            st.session_state.logs = logs_df.to_dict('records')
-        except FileNotFoundError:
-            st.session_state.logs = []
-                
-        page = st. sidebar.radio('Select page', ['STG-2024', 'View Logs'])
-        
-        if page == 'STG-2024':
-            def main():
-                st.markdown("""
-                <style>
-                    .stProgress > div > div > div {
-                        background-color: #FFD700;
-                        border-radius: 50%;
-                    }
-                </style>
-                """, unsafe_allow_html=True)
-                
-                with st.spinner("Data loaded successfully!"):
-                    import time
-                    time.sleep(1)
-                
-                col1, col2 = st.columns([2, 0.75])
-                with col1:
-                    st.markdown("""
-                        <h2 style='text-align: center; font-size: 40px; color: red;'>
-                            Find your parts
-                        </h2>
-                    """, unsafe_allow_html=True)
-                
-                with col2:
-                    search_keyword = st.session_state.get('search_keyword', '')
-                    search_keyword = st.text_input("Enter keyword to search:", search_keyword)
-                    search_button = st.button("Search")
-                    search_option = 'All Columns'
-                
-                def search_in_dataframe(df_Material, keyword, option):
-                    if option == 'All Columns':
-                        result = df_Material[df_Material.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
-                    else:
-                        result = df_Material[df_Material[option].astype(str).str.contains(keyword, case=False)]
-                    return result
-                
-                if st.session_state.get('refreshed', False):
-                    st.session_state.search_keyword = ''
-                    st.session_state.refreshed = False
-                
-                if search_button and search_keyword:
-                    st.session_state.search_keyword = search_keyword
-                    search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
-                    st.write(f"Search results for '{search_keyword}' in {search_option}:")
-                    st.dataframe(search_results, width=1000, height=200)
-                st.session_state.refreshed = True 
-                
-                tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
-                    'Reel Label (Small)', 'Reel Label (Large)',
-                    'Ink Reels for Label', 'Red Tape', 'Adhasive Tape', 'Cartridges', 'MultiPharma Cartridge'
-                ])
-                
-                with tab1:
-                    Small = df_Material[df_Material['Item Name'] == 'Reel Label (Small)'].sort_values(by='Item Name')
-                    st.dataframe(Small,width=2000)
-                    col4, col5, col6 = st.columns([2,1,2])
-                    with col4:
-                        display_tab('Reel Label (Small)', 20)
-                   
-                with tab2:
-                    Large = df_Material[df_Material['Item Name'] == 'Reel Label (Large)'].sort_values(by='Item Name')
-                    st.dataframe(Large,width=2000)
-                    col4, col5, col6 = st.columns([2,1,2])
-                    with col4:
-                        display_tab('Reel Label (Large)', 60)
-                with tab3:
-                    Ink = df_Material[df_Material['Item Name'] == 'Ink Reels for Label'].sort_values(by='Item Name')
-                    st.dataframe(Ink,width=2000)
-                    col4, col5, col6 = st.columns([2,1,2])
-                    with col4:
-                        display_tab('Ink Reels for Label', 20)
-                with tab4:
-                    Tape = df_Material[df_Material['Item Name'] == 'Red Tape'].sort_values(by='Item Name')
-                    st.dataframe(Tape,width=2000)
-                    col4, col5, col6 = st.columns([2,1,2])
-                    with col4:
-                        display_tab('Red Tape', 5)
-                with tab5:
-                    Adhasive = df_Material[df_Material['Item Name'] == 'Adhasive Tape'].sort_values(by='Item Name')
-                    st.dataframe(Adhasive,width=2000)
-                    col4, col5, col6 = st.columns([2,2,2])
-                    with col4:
-                        display_tab('Adhasive Tape', 100)
-                with tab6:
-                    Cartridges = df_Material[df_Material['Item Name'] == 'Cartridges'].sort_values(by='Item Name')
-                    st.dataframe(Cartridges,width=2000)
-                    col4, col5, col6 = st.columns([2,1,2])
-                    with col4:
-                        display_tab('Cartridges', 50)
-                with tab7:
-                    MultiPharma = df_Material[df_Material['Item Name'] == 'MultiPharma Cartridge'].sort_values(by='Item Name')
-                    st.dataframe(MultiPharma,width=2000)
-                    col4, col5, col6 = st.columns([2,1,2])
-                    with col4:
-                        display_tab('MultiPharma Cartridge', 5)
+    st.markdown(f"<div style='text-align: right; font-size: 20px; color: green;'>Logged in by: {users[st.session_state.username]['name']}</div>", unsafe_allow_html=True)
+    
+    # Load data
+    if 'df' not in st.session_state:
+        st.session_state.df = pd.read_csv('matril.csv')
+    try:
+        logs_df = pd.read_csv('logs.csv')
+        st.session_state.logs = logs_df.to_dict('records')
+    except FileNotFoundError:
+        st.session_state.logs = []
 
-                st.button("updated page")
-                csv = df_Material.to_csv(index=False)
-                st.download_button(label="Download updated sheet", data=csv, file_name='updated.csv', mime='text/csv')
-        
-                
-        
-            if __name__ == '__main__':
-                main()
-        elif page == 'View Logs':
-            st.header('User Activity Logs')
-            if st.session_state.logs:
-                logs_df = pd.DataFrame(st.session_state.logs)
-                st.dataframe(logs_df, width=1000, height=400)
-                csv = logs_df.to_csv(index=False)
-                st.download_button(label="Download Logs as CSV", data=csv, file_name='user_logs.csv', mime='text/csv')
-                if st.button("Clear Logs"):
-                    clear_logs()
+    page = st.sidebar.radio('Select page', ['STG-2024', 'View Logs'])
+    
+    if page == 'STG-2024':
+        def main():
+            st.markdown("""
+            <style>
+                .stProgress > div > div > div {
+                    background-color: #FFD700;
+                    border-radius: 50%;
+                }
+            </style>
+            """, unsafe_allow_html=True)
             
-            else:
-                st.write("No logs available.")
+            with st.spinner("Data loaded successfully!"):
+                import time
+                time.sleep(1)
+            
+            col1, col2 = st.columns([2, 0.75])
+            with col1:
+                st.markdown("""
+                    <h2 style='text-align: center; font-size: 40px; color: red;'>
+                        Find your parts
+                    </h2>
+                """, unsafe_allow_html=True)
+            
+            with col2:
+                search_keyword = st.session_state.get('search_keyword', '')
+                search_keyword = st.text_input("Enter keyword to search:", search_keyword)
+                search_button = st.button("Search")
+                search_option = 'All Columns'
+            
+            def search_in_dataframe(df_Material, keyword, option):
+                if option == 'All Columns':
+                    result = df_Material[df_Material.apply(lambda row: row.astype(str).str.contains(keyword, case=False).any(), axis=1)]
+                else:
+                    result = df_Material[df_Material[option].astype(str).str.contains(keyword, case=False)]
+                return result
+            
+            if st.session_state.get('refreshed', False):
+                st.session_state.search_keyword = ''
+                st.session_state.refreshed = False
+            
+            if search_button and search_keyword:
+                st.session_state.search_keyword = search_keyword
+                search_results = search_in_dataframe(st.session_state.df, search_keyword, search_option)
+                st.write(f"Search results for '{search_keyword}'in{search_option}:")
+                st.dataframe(search_results, width=1000, height=200)
+            st.session_state.refreshed = True 
+            
+            tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs([
+                'Reel Label (Small)', 'Reel Label (Large)',
+                'Ink Reels for Label', 'Red Tape', 'Adhesive Tape', 'Cartridges', 'MultiPharma Cartridge'
+            ])
+            
+            with tab1:
+                Small = df_Material[df_Material['Item Name'] == 'Reel Label (Small)'].sort_values(by='Item Name')
+                st.dataframe(Small, width=2000)
+                col4, col5, col6 = st.columns([2,1,2])
+                with col4:
+                    display_tab('Reel Label (Small)', 20)
+               
+            with tab2:
+                Large = df_Material[df_Material['Item Name'] == 'Reel Label (Large)'].sort_values(by='Item Name')
+                st.dataframe(Large, width=2000)
+                col4, col5, col6 = st.columns([2,1,2])
+                with col4:
+                    display_tab('Reel Label (Large)', 60)
+            with tab3:
+                Ink = df_Material[df_Material['Item Name'] == 'Ink Reels for Label'].sort_values(by='Item Name')
+                st.dataframe(Ink, width=2000)
+                col4, col5, col6 = st.columns([2,1,2])
+                with col4:
+                    display_tab('Ink Reels for Label', 20)
+            with tab4:
+                Tape = df_Material[df_Material['Item Name'] == 'Red Tape'].sort_values(by='Item Name')
+                st.dataframe(Tape, width=2000)
+                col4, col5, col6 = st.columns([2,1,2])
+                with col4:
+                    display_tab('Red Tape', 5)
+            with tab5:
+                Adhesive = df_Material[df_Material['Item Name'] == 'Adhesive Tape'].sort_values(by='Item Name')
+                st.dataframe(Adhesive, width=2000)
+                col4, col5, col6 = st.columns([2,2,2])
+                with col4:
+                    display_tab('Adhesive Tape', 100)
+            with tab6:
+                Cartridges = df_Material[df_Material['Item Name'] == 'Cartridges'].sort_values(by='Item Name')
+                st.dataframe(Cartridges, width=2000)
+                col4, col5, col6 = st.columns([2,1,2])
+                with col4:
+                    display_tab('Cartridges', 50)
+            with tab7:
+                MultiPharma = df_Material[df_Material['Item Name'] == 'MultiPharma Cartridge'].sort_values(by='Item Name')
+                st.dataframe(MultiPharma, width=2000)
+                col4, col5, col6 = st.columns([2,1,2])
+                with col4:
+                    display_tab('MultiPharma Cartridge', 5)
+
+            st.button("Update page")
+            csv = df_Material.to_csv(index=False)
+            st.download_button(label="Download updated sheet", data=csv, file_name='updated.csv', mime='text/csv')
+    
+        if __name__ == '__main__':
+            main()
+    elif page == 'View Logs':
+        st.header('User Activity Logs')
+        if st.session_state.logs:
+            logs_df = pd.DataFrame(st.session_state.logs)
+            st.dataframe(logs_df, width=1000, height=400)
+            csv = logs_df.to_csv(index=False)
+            st.download_button(label="Download Logs as CSV", data=csv, file_name='user_logs.csv', mime='text/csv')
+            if st.button("Clear Logs"):
+                clear_logs()
+        
+        else:
+            st.write("No logs available.")
